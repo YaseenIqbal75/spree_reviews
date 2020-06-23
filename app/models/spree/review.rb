d007f2fef483d1dca839cf1ce1aef82b69ab128d
Umer farzad <umerfarzand@gmail.com>
2020-06-23 23:46:09 +0500

    feat(multilingual): update static text.

--- app/models/spree/review.rb
+++ app/models/spree/review.rb
@@ -6,13 +6,13 @@ class Spree::Review < ActiveRecord::Base
   after_save :recalculate_product_rating, if: :approved?
   after_destroy :recalculate_product_rating
 
-  validates :name, presence: { message: 'must not be blank' }
-  validates :review, presence: { message: 'must not be blank' }
+  validates :name, presence: { message: Spree.t('pages.product_detail.review.common_error') }
+  validates :review, presence: { message: Spree.t('pages.product_detail.review.common_error') }
   validates :rating, numericality: {
     only_integer: true,
     greater_than_or_equal_to: 1,
     less_than_or_equal_to: 5,
-    message: Spree.t(:you_must_enter_value_for_rating)
+    message: Spree.t('pages.product_detail.review.rating_error')
   }
 
   default_scope { order('spree_reviews.created_at DESC') }
